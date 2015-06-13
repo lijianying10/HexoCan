14f871eac06521ba986dc9a1de6e7514a50d3fbf
lijianying12 <lijianying12@gmail.com>
2015-06-13 20:51:38 +0800

    update build script ,rootfs maker , rootfs ,Dockerfile

--- makerootfs.sh
+++ makerootfs.sh
@@ -13,6 +13,9 @@ cp $(which git) bin/
 $(which busybox) --install -s bin #同上两步操作，用which是因为需要绝对路径
 bash -c "cp /lib/x86_64-linux-gnu/lib{c,z,pcre,m,dl,crypt,util,rt,nsl,nss_*,pthread,resolv}.so.* lib" # 这一步很重要，如果使用了Go调用外部动态库这里需要复制进去，
 bash -c "cp /lib/x86_64-linux-gnu/ld* lib" #同上
+bash -c "mkdir -p usr/lib/git-core/" #同上
+bash -c "cp -rf /usr/lib/git-core/* usr/lib/git-core/" #同上
+
 cp /lib/x86_64-linux-gnu/ld-linux-x86-64.so.2 lib #同上
 tar cf /rootfs.tar . #根目录打包
 for X in console null ptmx random stdin stdout stderr tty urandom zero ; do tar uf /rootfs.tar -C/ ./dev/$X ; done 
